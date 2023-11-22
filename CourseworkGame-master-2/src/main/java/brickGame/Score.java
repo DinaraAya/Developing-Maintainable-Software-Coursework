5d4f52adeb59acf6405d82ef8e7e30343cbfa5f5
DinaraAya <dinaraayaganovua@gmail.com>
2023-11-23 00:34:15 +0800

    gameover scene and score display

--- CourseworkGame-master-2/src/main/java/brickGame/Score.java
+++ CourseworkGame-master-2/src/main/java/brickGame/Score.java
@@ -1,13 +1,34 @@
 package brickGame;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+
+
 //import sun.plugin2.message.Message;
 
 public class Score {
+    private Rectangle highScoreRect;
+    private Rectangle scoreRect;
+
+    private Stage primaryStage;
+    
+
     public void show(final double x, final double y, int score, final Main main) {
         String sign;
         if (score >= 0) {
@@ -15,9 +36,9 @@ public class Score {
         } else {
             sign = "";
         }
-        // final Label label = new Label(sign + score);
-        // label.setTranslateX(x);
-        // label.setTranslateY(y);
+        final Label label = new Label(sign + score);
+        label.setTranslateX(x);
+        label.setTranslateY(y);
 
         // Platform.runLater(new Runnable() {
         //     @Override
@@ -26,6 +47,7 @@ public class Score {
         //     }
         // });
 
+        
         // new Thread(new Runnable() {
         //     @Override
         //     public void run() {
@@ -39,64 +61,138 @@ public class Score {
         //                 e.printStackTrace();
         //             }
         //         }
+        //         main.root.getChildren().remove(label);
         //     }
         // }).start();
     }
 
     public void showMessage(String message, final Main main) {
-        final Label label = new Label(message);
-        label.setTranslateX(220);
-        label.setTranslateY(340);
+        Platform.runLater(() -> {
+            Label label = new Label(message);
+            label.setTranslateX(220);
+            label.setTranslateY(340);
+            
+            main.root.getChildren().add(label);
 
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                main.root.getChildren().add(label);
-            }
-        });
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
+            new Thread(() -> {
                 for (int i = 0; i < 21; i++) {
                     try {
-                        label.setScaleX(Math.abs(i-10));
-                        label.setScaleY(Math.abs(i-10));
-                        label.setOpacity((20 - i) / 20.0);
+                        final int finalI = i;
+                        Platform.runLater(() -> {
+                            label.setScaleX(Math.abs(finalI - 10));
+                            label.setScaleY(Math.abs(finalI - 10));
+                            label.setOpacity((20 - finalI) / 20.0);
+                        });
                         Thread.sleep(15);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
-            }
-        }).start();
-    }
-
-    public void showGameOver(final Main main) {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                Label label = new Label("Game Over :(");
-                label.setTranslateX(200);
-                label.setTranslateY(250);
-                label.setScaleX(2);
-                label.setScaleY(2);
-
-                Button restart = new Button("Restart");
-                restart.setTranslateX(220);
-                restart.setTranslateY(300);
-                restart.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        main.restartGame();
-                    }
-                });
-
-                main.root.getChildren().addAll(label, restart);
-
-            }
+            }).start();
         });
     }
+
+    private int readHighestScore() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("highscore.txt"))) {
+            String line = reader.readLine();
+            if (line != null) {
+                return Integer.parseInt(line);
+            }
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getHighestScore() {
+        return readHighestScore();
+    }
+
+
+    public void showGameOver(final Main main, int score) {
+        Platform.runLater(() -> {
+
+            int highestScore = readHighestScore();
+            if(score > highestScore) {
+                highestScore = score;
+                saveHighestScore(highestScore);
+            }
+
+            Label label = new Label("GAME OVER");
+            label.getStyleClass().add("bigText");
+            label.setTranslateX(180);
+            label.setTranslateY(210);
+            label.setScaleX(2);
+            label.setScaleY(2);
+
+            Image highScoreImage = new Image("highscore.png");
+            ImagePattern highScorePattern = new ImagePattern(highScoreImage);
+            highScoreRect = new Rectangle(200, 130);
+            highScoreRect.setTranslateY(260);
+            highScoreRect.setTranslateX(40);
+            highScoreRect.setFill(highScorePattern);
+
+            Image scoreImage = new Image("score.png");
+            ImagePattern scoreImagePattern = new ImagePattern(scoreImage);
+            scoreRect = new Rectangle(200, 130);
+            scoreRect.setTranslateY(260);
+            scoreRect.setTranslateX(248);
+            scoreRect.setFill(scoreImagePattern);
+
+            Label highScoreLabel = new Label("SCORE");
+            highScoreLabel.getStyleClass().add("text");
+            highScoreLabel.setStyle("-fx-font-size: 16px");
+            highScoreLabel.setTranslateX(90);
+            highScoreLabel.setTranslateY(260);
+
+            Label score1 = new Label(String.valueOf(score));
+            score1.getStyleClass().add("bigText");
+            score1.setStyle("-fx-font-size: 36px");
+            score1.setTranslateX(105);
+            score1.setTranslateY(310);
+
+            Label score2 = new Label(String.valueOf(highestScore));
+            score2.getStyleClass().add("bigText");
+            score2.setStyle("-fx-font-size: 36px");
+            score2.setTranslateX(310);
+            score2.setTranslateY(310);
+
+            Label ScoreLabel = new Label("BEST");
+            ScoreLabel.getStyleClass().add("text");
+            ScoreLabel.setStyle("-fx-font-size: 16px");
+            ScoreLabel.setTranslateX(310);
+            ScoreLabel.setTranslateY(260);
+            
+            Button restart = new Button("Restart");
+            restart.setTranslateX(220);
+            restart.setTranslateY(300);
+            restart.setOnAction(event -> main.goHome());
+
+            Image restartImage = new Image("restart.png");
+            ImageView restartView = new ImageView(restartImage);
+            restart = new Button("");
+            restart.setId("pause-button");
+            restart.setPrefSize(65, 55);
+            restart.setTranslateX(210);
+            restart.setTranslateY(420);
+            restart.setGraphic(restartView);
+
+            restart.setOnAction(event -> {
+                main.goHome();
+            });
+
+            main.gameOverRoot.getChildren().addAll(label, highScoreRect, scoreRect, highScoreLabel, ScoreLabel, score1, score2, restart);
+        });
+    }
+
+    private void saveHighestScore(int highestScore) {
+        try(FileWriter writer = new FileWriter("highscore.txt")) {
+            writer.write(String.valueOf(highestScore));
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public void showWin(final Main main) {
         Platform.runLater(new Runnable() {
@@ -107,11 +203,10 @@ public class Score {
                 label.setTranslateY(250);
                 label.setScaleX(2);
                 label.setScaleY(2);
-
-
                 main.root.getChildren().addAll(label);
 
             }
         });
     }
 }
+
