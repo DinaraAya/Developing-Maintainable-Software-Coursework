0c78cd73d8c38ad60a7c0b20916ba76c1ee7b3cc
DinaraAya <147474129+DinaraAya@users.noreply.github.com>
2023-12-03 16:35:24 +0800

    Delete CourseworkGame-master-2 directory

--- CourseworkGame-master-2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module brickGame {
-    requires javafx.fxml;
-    requires transitive javafx.controls;
-    requires javafx.media;
-    requires javafx.graphics;
-
-    opens brickGame to javafx.fxml, javafx.graphics;
-    exports brickGame;
-}
